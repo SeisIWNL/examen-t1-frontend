a1c44562f8b54a6fb3112c9185287e8247e679a1
Seis <ivanrup123@gmail.com>
2024-09-26 19:11:17 -0500

    Primera carga de archivos

---
+++ src/main/java/pe/edu/cibertec/VehiculoFront/VehiculoFrontApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.VehiculoFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculoFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculoFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/VehiculoFront/VehiculoFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.VehiculoFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculoFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
